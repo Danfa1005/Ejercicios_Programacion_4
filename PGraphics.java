127beb0584b75c7c2b3c7069ff249ff1379448bc
JesusMG17 <68879190+JesusMG17@users.noreply.github.com>
2020-08-30 23:52:27 -0500

    Update PGraphics.java

--- PGraphics.java
+++ PGraphics.java
@@ -1,4 +1,29 @@
 
 public class PGraphics {
 
+	public void beginDraw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void background(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void strokeWeight(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void endDraw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void line(float old_x, float old_y, float drawX, float drawY) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }

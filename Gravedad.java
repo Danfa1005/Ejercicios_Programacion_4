dbf956b3d301e28eb592d7faa653c8dcd92db3b1
JesusMG17 <68879190+JesusMG17@users.noreply.github.com>
2020-08-30 23:51:38 -0500

    Update Gravedad.java

--- Gravedad.java
+++ Gravedad.java
@@ -1,61 +1,121 @@
 
-class Pendulo {
-		  private static final int PI = 0;
-		float angulo, cuerda, masa, vel, x, y;
-		  
-		  Pendulo(){
-		   this(200, 10, PI/4);
-		  }
-		  
-		  Pendulo(float r1, float m1, float a1){
-		    cuerda = r1;
-		    masa = m1;
-		    angulo = a1;
-		    acelerar(0, 1);
-		  }
-		   
-		  void acelerar(float accel, float frict){
-		    vel += accel;
-		    angulo += vel;
-		    vel *= frict;
-		    x = cuerda * sin(angulo);
-		    y = cuerda * cos(angulo);
-		  }
-		  
-		  
-		  
-		  private float cos(float angulo2) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
+public class Gravedad {
+	private static final int PI = 0;
+	final float gravedad = (float) 1.1;
+	final float frict = (float) 0.9999;
 
-		private float sin(float angulo2) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
+	Pendulo p1, p2;
 
-		void dibujar(float x_off, float y_off){
-		    line(x_off, y_off, x + x_off, y + y_off);
-		    ellipse(x + x_off, y + y_off, 5*log(masa), 5*log(masa));
-		  }
-		  
-		  private void ellipse(float f, float g, int i, int j) {
-			// TODO Auto-generated method stub
-			
-		}
+	final int cx = 700, cy = 700;
+	int anchorX, anchorY;
+	boolean draw = false;
 
-		private int log(float masa2) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
+	PGraphics pg;
+	float old_x, old_y;
 
-		private void line(float x_off, float y_off, float f, float g) {
-			// TODO Auto-generated method stub
-			
-		}
+	void settings(){
+	  size(cx, cy);
+	  int width = 0;
+	anchorX = width/2;
+	  int height = 0;
+	anchorY = height/2;
+	}
 
-		void dibujar(){
-		     dibujar(0, 0); 
-		  }
-		  
-		}
+	private void size(int cx2, int cy2) {
+		
+	}
+
+	void setup(){
+	  // Crear péndulos
+	  p1 = new Pendulo(150, 25, random(0, 2*PI));
+	  p2 = new Pendulo(150, 25, random(0, 2*PI));
+	 
+	  stroke(0);
+	  fill(0);
+	   
+	  pg = createGraphics(cx, cy); 
+	  pg.beginDraw();
+	  pg.background(255);
+	  pg.strokeWeight(1);
+	  pg.strokeWeight(150);
+	  pg.endDraw();
+	  
+	   draw = false;
+	}
+
+	private PGraphics createGraphics(int cx2, int cy2) {
+		return null;
+	}
+
+	private void fill(int i) {
+		
+	}
+
+	private void stroke(int i) {
+		
+	}
+
+	private float random(int i, int j) {
+		return 0;
+	}
+
+	void draw(){
+	 background(255);
+	 image(pg, 0, 0);
+	 
+	 //Mover al punto de anclaje
+	 translate(anchorX, anchorY);
+
+	 // Calcular aceleración de péndulos
+	 float calculo = (-gravedad * (2*p1.masa + p2.masa) * sin(p1.angulo) -
+	   p2.masa * gravedad * sin(p1.angulo - 2*p2.angulo) - 2 * sin(p1.angulo - p2.angulo) * 
+	   p2.masa * (p2.vel * p2.vel * p2.cuerda + p1.vel * p1.vel * p1.cuerda * cos(p1.angulo - p2.angulo)))
+	   / (p1.cuerda * (2*p1.masa + p2.masa - p2.masa * cos(2*p1.angulo - 2*p2.angulo))); 
+	   
+	 float calculo2 = 2 * sin(p1.angulo - p2.angulo) * ( p1.vel * p1.vel * p1.cuerda * (p1.masa + p2.masa)
+	   + gravedad * (p1.masa + p2.masa) * cos(p1.angulo) + p2.vel * p2.vel * p2.cuerda * p2.masa * cos(p1.angulo - p2.angulo))
+	   / (p2.cuerda * (2*p1.masa + p2.masa - p2.masa * cos(2*p1.angulo - 2*p2.angulo)));
+	   
+	  // Acelerar y dibujar
+	  p1.acelerar(calculo, frict);
+	  p2.acelerar(calculo2, frict);
+	  p1.dibujar();
+	  p2.dibujar(p1.x, p1.y);
+	  
+	  //Dibujar línea de trazo
+	  float drawX = p2.x + p1.x + anchorX;
+	  float drawY = p2.y + p1.y + anchorY;
+	  pg.beginDraw();
+	  if(draw){
+	    pg.line(old_x, old_y, drawX, drawY);
+	  } else draw = true;
+	  pg.endDraw();
+	  old_x = drawX;
+	  old_y = drawY;
+	  
+	}
+
+	private float cos(float f) {
+		return 0;
+	}
+
+	private float sin(float angulo) {
+		return 0;
+	}
+
+	private void translate(int anchorX2, int anchorY2) {
+		
+	}
+
+	private void image(PGraphics pg2, int i, int j) {
+		
+	}
+
+	private void background(int i) {
+		
+	}
+
+	void mouseClicked(){
+	  setup();
+	}
+}
